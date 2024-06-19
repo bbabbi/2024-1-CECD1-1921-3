96a0abafa498375af04ff56105c6ab4eab58eee1
bbabbi <mooncheayoung@gmail.com>
2024-06-20 02:53:41 +0900

    [FIX] : react router 적용되지 않는 오류 수정을 위해 파일 내용 수정

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,13 +5,17 @@ import GlobalStyles from './styles/GlobalStyle';
 import theme from './styles/theme';
 import Router from './Router';
 import ControlPanel from './components/ControlPanel';
-import exampleData from './data/exampleData';
+import exampleData from './data/ControlExData';
+import WarningLogPanel from './components/WarningLogPanel';
+import warningLogData from './data/WarningLogExData';
 
 const App: React.FC = () => {
   return (
 		<ThemeProvider theme={theme}>
 			<GlobalStyles />
+			<Router />
       <ControlPanel {...exampleData} />
+			<WarningLogPanel {...warningLogData} />
 		</ThemeProvider>
   );
 };
--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,8 +1,11 @@
 // src/index.tsx
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
+import './styles/App.css';
 import reportWebVitals from './reportWebVitals';
+import './index.css';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -12,3 +15,14 @@ ReactDOM.render(
 );
 
 reportWebVitals();
+
+// const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+// root.render(
+//   <React.StrictMode>
+//     <Router>
+//       <App />
+//     </Router>
+//   </React.StrictMode>
+// );
+
+
