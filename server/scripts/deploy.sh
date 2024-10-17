5a2bda354f605f09883eb584463c1d183498295e
bbabbi <mooncheayoung@gmail.com>
2024-10-18 02:49:43 +0900

    #11 [FEAT] : deploy.sh 수정

--- server/scripts/deploy.sh
+++ server/scripts/deploy.sh
@@ -1,94 +1,87 @@
-nginx_config_path="/etc/nginx"
-all_port=("8080" "8081")
-available_port=()
-user_name=chaeyoungmoon
-server_name=cecd1921
+#!/bin/bash
+BUILD_PATH=$(ls /home/ubuntu/app/server-0.0.1-SNAPSHOT.jar)
+JAR_NAME=$(basename $BUILD_PATH)
+echo "> build 파일명: $JAR_NAME"
 
-docker_ps_output=$(docker ps | grep $server_name)
-running_container_name=$(echo "$docker_ps_output" | awk '{print $NF}')
-blue_port=$(echo "$running_container_name" | awk -F'-' '{print $NF}')
-web_health_check_url=/actuator/health
+echo "> build 파일 복사"
+DEPLOY_PATH=/home/ubuntu/app/
+cp $BUILD_PATH $DEPLOY_PATH
 
+echo "> 현재 구동중인 Port 확인"
+BLUE_PROFILE=$(curl -s http://localhost/profile)
+echo "> $BLUE_PROFILE"
 
-if [ -z "$blue_port" ]; then
-    echo "> 실행 중인 서버의 포트: 없음"
+# 쉬고 있는 set 찾기: set1이 사용중이면 set2가 쉬고 있고, 반대면 set1이 쉬고 있음
+if [ $BLUE_PROFILE == set1 ]
+then
+  GREEN_PROFILE=set2
+  GREEN_PORT=8082
+elif [ $BLUE_PROFILE == set2 ]
+then
+  GREEN_PROFILE=set1
+  GREEN_PORT=8081
 else
-    echo "> 실행 중인 서버의 포트: $blue_port"
+  echo "> 일치하는 Profile이 없습니다. Profile: $BLUE_PROFILE"
+  echo "> set1을 할당합니다. IDLE_PROFILE: set1"
+  GREEN_PROFILE=set1
+  GREEN_PORT=8081
 fi
 
-# 실행 가능한 포트 확인 ( all_port 중 blue_port를 제외한 port )
-for item in "${all_port[@]}"; do
-    if [ "$item" != "$blue_port" ]; then
-        available_port+=("$item")
-    fi
-done
+echo "> application.jar 교체"
+GREEN_APPLICATION=$GREEN_PROFILE-server.jar
+GREEN_APPLICATION_PATH=$DEPLOY_PATH$GREEN_APPLICATION
 
-if [ ${#available_port[@]} -eq 0 ]; then
-    echo "> 실행 가능한 포트가 없습니다."
-    exit 1
-fi
-
-green_port=${available_port[0]}
+ln -Tfs $DEPLOY_PATH$JAR_NAME $GREEN_APPLICATION_PATH
 
 echo "----------------------------------------------------------------------"
-# docker image pull
-echo "> 도커 이미지 pull 받기"
-docker pull ${user_name}/${server_name}
+echo "> $GREEN_PROFILE 배포"
+nohup java -jar -Duser.timezone=Asia/Seoul -Dspring.profiles.active=$GREEN_PROFILE $GREEN_APPLICATION_PATH >> /home/ubuntu/app/nohup.out 2>&1 &
 
-echo "> ${green_port} 포트로 서버 실행"
-echo "> docker run -d --name ${server_name} -p ${green_port}:8080 -e TZ=Asia/Seoul ${user_name}/${server_name}"
-docker run -d --name ${server_name}-${green_port} -v /app -p ${green_port}:8080 -e TZ=Asia/Seoul ${user_name}/${server_name}
 echo "----------------------------------------------------------------------"
 
-sleep 10
-for retry_count in {1..10}
-do
-    echo "> 서버 상태 체크"
-    echo "> curl -s http://localhost:${green_port}${web_health_check_url}"
-    response=$(curl -s http://localhost:${green_port}${web_health_check_url})
-    up_count=$(echo $response | grep 'UP' | wc -l)
-
-    if [ $up_count -ge 1 ]
-    then
-        echo "> 서버 실행 성공"
-        break
-    else
-        echo "> 아직 서버 실행 안됨"
-        echo "> 응답 결과: ${response}"
-    fi
-    if [ $retry_count -eq 10 ]
-		then
-        echo "> 서버 실행 실패"
-        docker rm -f ${server_name}-${green_port}
-
-        exit 1
-    fi
-    sleep 5
-done
-echo "----------------------------------------------------------------------"
-# nginx switching
 echo "> nginx 포트 스위칭"
-echo "set \$service_url http://127.0.0.1:${green_port};" | sudo tee ${nginx_config_path}/conf.d/service-url.inc
+echo "set \$service_url http://127.0.0.1:${GREEN_PORT};" | sudo tee /etc/nginx/conf.d/service-url.inc
 sudo nginx -s reload
 
-sleep 1
+echo "> $GREEN_PROFILE 10초 후 Health check 시작"
+echo "> curl -s http://localhost:$GREEN_PORT/actuator/health "
+sleep 10
 
-echo "----------------------------------------------------------------------"
+for retry_count in {1..10}
+do
+  response=$(curl -s http://localhost:$GREEN_PORT/actuator/health)
+  up_count=$(echo $response | grep 'UP' | wc -l)
 
-response=$(curl -s http://localhost${web_health_check_url})
-up_count=$(echo $response | grep 'UP' | wc -l)
-if [ $up_count -ge 1 ]
-then
-    echo "> 서버 변경 성공"
-else
-    echo "> 서버 변경 실패"
-    echo "> 서버 응답 결과: ${response}"
+  if [ $up_count -ge 1 ]
+  then
+      echo "> Health check 성공"
+
+      break
+  else
+      echo "> Health check의 응답을 알 수 없거나 혹은 status가 UP이 아닙니다."
+      echo "> Health check: ${response}"
+  fi
+
+  if [ $retry_count -eq 10 ]
+  then
+    echo "> Health check 실패. "
+    echo "> Nginx에 연결하지 않고 배포를 종료합니다."
     exit 1
-fi
+  fi
 
-if [ -n "$blue_port" ]; then
-    echo "> 기존 ${blue_port}포트 서버 중단"
-    echo "> docker rm -f ${server_name}-${blue_port}"
-    sudo docker rm -f ${server_name}-${blue_port}
-		docker rmi $(docker images -f "dangling=true" -q)
+  echo "> Health check 연결 실패. 재시도..."
+  sleep 10
+done
+
+echo "> $BLUE_PROFILE 에서 구동중인 애플리케이션 pid 확인"
+BLUE_PID=$(pgrep -f $BLUE_PROFILE-server.jar)
+
+if [ -z $BLUE_PID ]
+then
+  echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다."
+else
+  echo "> 기존 ${BLUE_PROFILE} 서버 중단"
+  echo "> kill -15 $BLUE_PID"
+  kill -15 $BLUE_PID
+  sleep 5
 fi
