71ccd552bca6b6cd2468cfa1a50a07aa14c04102
bbabbi <mooncheayoung@gmail.com>
2024-11-01 04:26:03 +0900

    #27 [CHORE] : Device Entity 어노테이션 수정

--- server/src/main/java/org/cecd/server/domain/Device.java
+++ server/src/main/java/org/cecd/server/domain/Device.java
@@ -1,7 +1,16 @@
 package org.cecd.server.domain;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+@Entity
+@Builder
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Device {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY) @Column(name = "device_id")
