d6ae02ad5daa2b98d203763bf9dc0347f42462a0
bbabbi <mooncheayoung@gmail.com>
2024-11-06 16:53:41 +0900

    #29 [CHORE] : 어노테이션 수정

--- server/src/main/java/org/cecd/server/repository/DeviceRepository.java
+++ server/src/main/java/org/cecd/server/repository/DeviceRepository.java
@@ -2,9 +2,11 @@ package org.cecd.server.repository;
 
 import org.cecd.server.domain.Device;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface DeviceRepository extends JpaRepository<Device, Long> {
     List<Device> findByBuildingNameAndLocation(String buildingName, String location);
     List<Device> findByBuildingName(String buildingName);
