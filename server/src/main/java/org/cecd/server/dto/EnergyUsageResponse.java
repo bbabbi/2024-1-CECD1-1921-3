3ccf59cfd49869214d8a1d669a7175cc69d0b165
bbabbi <mooncheayoung@gmail.com>
2024-11-09 21:20:27 +0900

    #29 [CHORE] : DTO record로 변경

--- server/src/main/java/org/cecd/server/dto/EnergyUsageResponse.java
+++ server/src/main/java/org/cecd/server/dto/EnergyUsageResponse.java
@@ -1,18 +1,10 @@
 package org.cecd.server.dto;
 
-
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.Setter;
-
 import java.time.LocalDateTime;
 
-@Getter
-@Setter
-@AllArgsConstructor
-public class EnergyUsageResponse {
-    private String sensorNumber;
-    private LocalDateTime dateTime;
-    private double result;
-    private double percentage;
-}
+public record EnergyUsageResponse(
+        String sensorNumber,
+        LocalDateTime dateTime,
+        double result,
+        double percentage
+) {}
