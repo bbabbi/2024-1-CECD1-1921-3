29935994256423a9bc76d41da4f330d550e8e84c
bbabbi <mooncheayoung@gmail.com>
2024-11-09 21:14:46 +0900

    #29 [FEAT] : findById 추가

--- server/src/main/java/org/cecd/server/repository/DeviceRepository.java
+++ server/src/main/java/org/cecd/server/repository/DeviceRepository.java
@@ -5,9 +5,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface DeviceRepository extends JpaRepository<Device, Long> {
     List<Device> findByBuildingNameAndLocation(String buildingName, String location);
     List<Device> findByBuildingName(String buildingName);
+    Optional<Device> findById(Long id);
 }
