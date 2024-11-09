af88c674c3ccb225b232604c13926678771c542d
bbabbi <mooncheayoung@gmail.com>
2024-11-09 21:48:43 +0900

    #33 [FEAT] : API가 호스팅 되는 서버 목록 정의

--- server/src/main/java/org/cecd/server/external/SwaggerConfig.java
+++ server/src/main/java/org/cecd/server/external/SwaggerConfig.java
@@ -1,10 +1,20 @@
 package org.cecd.server.external;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+@OpenAPIDefinition(
+        servers = {
+                @Server(url = "https://www.dgu1921.p-e.kr", description = "Dev Server"),
+                @Server(url = "http://localhost:8080", description = "Local Server")
+        }
+)
+
 
 @Configuration
 public class SwaggerConfig {
