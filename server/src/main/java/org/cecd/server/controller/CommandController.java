445598c6071b6d513ce1371e1dccbddaf87a0d87
bbabbi <mooncheayoung@gmail.com>
2024-11-20 13:59:56 +0900

    #30 [CHORE] : API URI 수정

--- server/src/main/java/org/cecd/server/controller/CommandController.java
+++ server/src/main/java/org/cecd/server/controller/CommandController.java
@@ -7,19 +7,19 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/command")
+@RequestMapping
 @RequiredArgsConstructor
 public class CommandController {
 
     private final CommandService commandService;
 
-    @PostMapping
+    @PostMapping("/control")
     public ResponseEntity<String> sendControlCommand(@RequestBody CommandRequest commandRequest) {
         String result = commandService.forwardToAgent(commandRequest);
         return ResponseEntity.ok(result);
     }
 
-    @PostMapping("/echo")
+    @PostMapping("/command")
     public ResponseEntity<CommandRequest> echoControlCommand(@RequestBody CommandRequest commandRequest) {
         return ResponseEntity.ok(commandRequest);
     }
